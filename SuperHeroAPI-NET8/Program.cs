cc4f36ac4b58703e8217a1af4980210b716ca2a7
Dhiraj Bhavsar <dheeraj_bhavsar@mohawkind.com>
2023-11-23 23:59:39 +0530

    Added telemetry

--- SuperHeroAPI-NET8/Program.cs
+++ SuperHeroAPI-NET8/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("Default");
+var aiConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"];
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -13,6 +14,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddDbContext<DataContext>(o => o.UseSqlServer(connectionString));
+builder.Services.AddApplicationInsightsTelemetry(o => o.ConnectionString = aiConnectionString);
 
 var app = builder.Build();
 
